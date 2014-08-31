d6a4b41340deafc92ddf4b25f3096bb74be229e3
Taro Omiya <taro.omiya@omiyagames.com>
2014-08-31 11:29:38 -0400

    Adding comments to the build script.

--- Assets/Editor/Omiya Games/OmiyaGamesBuildScript.cs
+++ Assets/Editor/Omiya Games/OmiyaGamesBuildScript.cs
@@ -5,10 +5,14 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
 
+/// <summary>
+/// Script that builds for a specific platform.  Requires Unity Pro license.
+/// Useful for continuous integration, like Jenkins.
+/// </summary>
 public class OmiyaGamesBuildScript
 {
 	/// <summary>
-	/// Cached string builder, useful for generating file names.	
+	/// Cached string builder, useful for generating file names.
 	/// </summary>
 	private static readonly StringBuilder FileNameGenerator = new StringBuilder();
 	/// <summary>
@@ -32,17 +36,25 @@ public class OmiyaGamesBuildScript
 	/// </summary>
 	private const string BuildDirectory = "Builds";
 	
+	/// <summary>
+	/// Function that builds for all platforms.  Edit this function if you want
+	/// to add more platforms besides PC, Mac, Linux, and Web.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build All")]
 	public static void BuildAllPlatforms()
 	{
+		// Build for the Web platform
 		PerformWebBuild();
 
+		// Build for the Windows platform
 		PerformWindows32Build();
 		PerformWindows64Build();
 
+		// Build for the Mac platform
 		PerformMac32Build();
 		PerformMac64Build();
 
+		// Build for the Linux platform
 		PerformLinux32Build();
 		PerformLinux64Build();
 
@@ -58,69 +70,103 @@ public class OmiyaGamesBuildScript
 			//PerformWp8Build();
 		}
 
+		// Build for the Android platform
 		//PerformAndroidBuild();
 	}
 	
+	/// <summary>
+	/// Function that builds for Web.  HTML page not included.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Web")]
 	public static void PerformWebBuild()
 	{
-		GenericBuild("Web", "", BuildTarget.WebPlayer);
+		GenericBuild("Web", ".unity3d", BuildTarget.WebPlayer);
 	}
 	
+	/// <summary>
+	/// Function that builds for Windows, 32-bit.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Windows 32-bit")]
 	public static void PerformWindows32Build()
 	{
 		GenericBuild("Windows 32-bit", ".exe", BuildTarget.StandaloneWindows);
 	}
 	
+	/// <summary>
+	/// Function that builds for Windows, 64-bit.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Windows 64-bit")]
 	public static void PerformWindows64Build()
 	{
 		GenericBuild("Windows 64-bit", ".exe", BuildTarget.StandaloneWindows64);
 	}
 	
+	/// <summary>
+	/// Function that builds for Mac, 32-bit.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Mac 32-bit")]
 	public static void PerformMac32Build()
 	{
 		GenericBuild("Mac 32-bit", ".app", BuildTarget.StandaloneOSXIntel);
 	}
 	
+	/// <summary>
+	/// Function that builds for Mac, 64-bit.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Mac 64-bit")]
 	public static void PerformMac64Build()
 	{
 		GenericBuild("Mac 64-bit", ".app", BuildTarget.StandaloneOSXIntel64);
 	}
 	
+	/// <summary>
+	/// Function that builds for Linux, 32-bit.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Linux 32-bit")]
 	public static void PerformLinux32Build()
 	{
 		GenericBuild("Linux 32-bit", "", BuildTarget.StandaloneLinux);
 	}
 	
+	/// <summary>
+	/// Function that builds for Linux, 64-bit.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Linux 64-bit")]
 	public static void PerformLinux64Build()
 	{
 		GenericBuild("Linux 64-bit", "", BuildTarget.StandaloneLinux64);
 	}
 	
+	/// <summary>
+	/// Function that builds for iOS.  Note this function only runs on a Mac.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/iOS")]
 	public static void PerformIosBuild()
 	{
 		GenericBuild("iOS", "", BuildTarget.iPhone);
 	}
 	
+	/// <summary>
+	/// Function that builds for Android.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Android")]
 	public static void PerformAndroidBuild()
 	{
 		GenericBuild("Android", ".apk", BuildTarget.Android);
 	}
 	
+	/// <summary>
+	/// Function that builds for Windows 8.  Note this function only runs on Windows 8.
+	/// </summary>
 	[MenuItem ("Omiya Games/Build For/Windows 8")]
 	public static void PerformWp8Build()
 	{
 		GenericBuild("Windows 8", "", BuildTarget.WP8Player);
 	}
 	
+	/// <summary>
+	/// Helper function that generates a build using a file name based off of the file extension.
+	/// </summary>
 	private static void GenericBuild(string platformName, string fileExtension, BuildTarget buildTarget)
 	{
 		// Sanitize the product name
@@ -158,6 +204,9 @@ public class OmiyaGamesBuildScript
 		GenericBuild(FileNameGenerator.ToString(), buildTarget);
 	}
 
+	/// <summary>
+	/// Helper function that generates a build.
+	/// </summary>
 	private static void GenericBuild(string targetDirectory, BuildTarget buildTarget)
 	{
 		// Import assets to this platform
@@ -178,6 +227,9 @@ public class OmiyaGamesBuildScript
 		}
 	}
 	
+	/// <summary>
+	/// Returns a list of scenes that are enabled in the build settings.
+	/// </summary>
 	private static string[] FindEnabledEditorScenes()
 	{
 		// Grab all enabled scenes
