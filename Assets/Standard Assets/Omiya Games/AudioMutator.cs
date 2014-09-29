fa1748f2152884141aee47ee006c3d76b434d6ec
Taro Omiya <taro.omiya@omiyagames.com>
2014-09-28 22:24:05 -0400

    Adding 3 more scripts: * ResetPlayerPrefsContextMenu adds a context menu (Omiya Games -> Reset Stored- Settings) that deletes all keys in player prefs. * AudioMutator randomizes the pitch of the audio source it's attached to every time you call Play() on the AudioMutator. * RandomList is a list that returns a random element when calling it's property, RandomElement. Like most music applications, the returned element will not be returned again until all elements in the list has been returned.

---
+++ Assets/Standard Assets/Omiya Games/AudioMutator.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioMutator : MonoBehaviour
+{
+    /// <summary>
+    /// The center pitch of this audio.
+    /// </summary>
+    [Range(-3, 3)]
+    public float centerPitch = 1;
+    /// <summary>
+    /// The allowed range the pitch can mutate from the center pitch
+    /// </summary>
+    public Vector2 pitchMutationRange = new Vector2(-0.5f, 0.5f);
+
+    AudioSource audioCache = null;
+
+    public AudioSource Audio
+    {
+        get
+        {
+            if(audioCache == null)
+            {
+                audioCache = audio;
+            }
+            return audioCache;
+        }
+    }
+
+    public void Play()
+    {
+        // Stop the audio
+        Audio.Stop();
+
+        // Change the audio's pitch
+        Audio.pitch = centerPitch + Random.Range(pitchMutationRange.x, pitchMutationRange.y);
+
+        // Play the audio
+        Audio.Play();
+    }
+
+    public void Play(float delaySeconds)
+    {
+        // Delay playing the audio
+        StartCoroutine(DelayPlay(delaySeconds));
+    }
+
+    public void Stop()
+    {
+        Audio.Stop();
+    }
+
+    void Awake()
+    {
+        // Update the pithes first
+        Audio.pitch = centerPitch;
+    }
+
+    IEnumerator DelayPlay(float delaySeconds)
+    {
+        yield return new WaitForSeconds(delaySeconds);
+        Play();
+    }
+}
---
+++ Assets/Standard Assets/Omiya Games/RandomList.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class RandomList<T> : List<T>
+{
+	int[] randomizedIndexes = null;
+	int index = int.MinValue;
+
+    public T CurrentElement
+    {
+        get
+        {
+            T returnElement = default(T);
+            if(Count > 0)
+            {
+                // Check if I need to setup a list
+                if(randomizedIndexes == null)
+                {
+                    SetupList();
+                    ShuffleList();
+                    index = 0;
+                }
+                else if(randomizedIndexes.Length != Count)
+                {
+                    SetupList();
+                    ShuffleList();
+                    index = 0;
+                }
+                else if((index >= randomizedIndexes.Length) || (index < 0))
+                {
+                    // Shuffle the list if we got to the last element
+                    ShuffleList();
+                    index = 0;
+                }
+
+                // Grab the current element
+                returnElement = this[randomizedIndexes[index]];
+            }
+            return returnElement;
+        }
+    }
+
+	public T RandomElement
+	{
+		get
+		{
+			T returnElement = default(T);
+			if(Count > 0)
+			{
+                ++index;
+                returnElement = CurrentElement;
+			}
+			return returnElement;
+		}
+	}
+
+    public void Reshuffle()
+    {
+        index = int.MinValue;
+    }
+
+    #region Helper Methods
+
+	void SetupList()
+	{
+		// Generate a new list, populated with entries based on frequency
+		randomizedIndexes = new int[Count];
+        for(index = 0; index < randomizedIndexes.Length; ++index)
+		{
+            randomizedIndexes[index] = index;
+		}
+    }
+    
+    void ShuffleList()
+	{
+        int randomIndex = 0, swapIndex = 0;
+        for(index = 0; index < randomizedIndexes.Length; ++index)
+		{
+			// Swap a random element
+			randomIndex = Random.Range(0, randomizedIndexes.Length);
+			if(index != randomIndex)
+			{
+				swapIndex = randomizedIndexes[index];
+				randomizedIndexes[index] = randomizedIndexes[randomIndex];
+				randomizedIndexes[randomIndex] = swapIndex;
+			}
+		}
+	}
+
+	#endregion
+}
