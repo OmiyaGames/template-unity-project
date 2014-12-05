66c0259bc2a05acc2dcb7c60924bc8c5ec17cdc2
taro <taro@localhost>
2014-12-05 17:22:26 -0500

    Working on setting up the main menu.

---
+++ Assets/Standard Assets/Omiya Games/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour
+{
+    bool isClicked = false;
+    
+    public void OnLevelClicked(int buttonNumber)
+    {
+        if ((isClicked == false) && (Singleton.Get<SceneTransition>().LoadLevel(buttonNumber) == true))
+        {
+            isClicked = true;
+        }
+    }
+
+    public void OnQuitClicked()
+    {
+        if (isClicked == false)
+        {
+            isClicked = true;
+            Application.Quit();
+        }
+    }
+}
