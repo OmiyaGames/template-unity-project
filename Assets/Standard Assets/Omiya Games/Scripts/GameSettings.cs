87b19c486bc45cc23fb1264f876805aef2a1aebb
Taro Omiya <taro.omiya@omiyagames.com>
2014-12-05 17:26:01 -0500

    Adding level unlocking.

--- Assets/Standard Assets/Omiya Games/Scripts/GameSettings.cs
+++ Assets/Standard Assets/Omiya Games/Scripts/GameSettings.cs
@@ -6,8 +6,11 @@ public class GameSettings : ISingletonScript
     public const int NumLevels = 7;
     public const int MenuLevel = 0;
     public const int CreditsLevel = NumLevels;
-	
+    public const string NumLevelsUnlockedKey = "numLevelsUnlocked";
+
 	public bool simulateWebplayer = false;
+    
+    int mNumLevelsUnlocked = 1;
 
 	public bool IsWebplayer
 	{
@@ -17,16 +20,17 @@ public class GameSettings : ISingletonScript
 		}
 	}
 
-	public int NumLevelsUnlocked
-	{
-		get
-		{
-			return NumLevels;
-		}
-		set
-		{
-		}
-	}
+    public int NumLevelsUnlocked
+    {
+        get
+        {
+            return mNumLevelsUnlocked;
+        }
+        set
+        {
+            mNumLevelsUnlocked = Mathf.Clamp(value, 1, NumLevels);
+        }
+    }
 	
 	public override void SingletonStart(Singleton instance)
 	{
@@ -36,16 +40,21 @@ public class GameSettings : ISingletonScript
     public override void SceneStart(Singleton instance)
 	{
 	}
-	
-	public void RetrieveFromSettings()
-	{
-	}
-	
-	public void SaveSettings()
-	{
-	}
-	
-	public void ClearSettings()
-	{
-	}
+
+    public void RetrieveFromSettings()
+    {
+        NumLevelsUnlocked = PlayerPrefs.GetInt(NumLevelsUnlockedKey, 1);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt(NumLevelsUnlockedKey, NumLevelsUnlocked);
+        PlayerPrefs.Save();
+    }
+
+    public void ClearSettings()
+    {
+        NumLevelsUnlocked = 1;
+        PlayerPrefs.DeleteAll();
+    }
 }
