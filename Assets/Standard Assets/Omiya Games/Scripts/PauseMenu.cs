a4565286797b700656bafcfafef3b796b132d4db
Taro Omiya <taro.omiya@omiyagames.com>
2014-12-05 19:31:11 -0500

    Adding the pause menu into the singleton.

--- Assets/Standard Assets/Omiya Games/Scripts/PauseMenu.cs
+++ Assets/Standard Assets/Omiya Games/Scripts/PauseMenu.cs
@@ -1,15 +1,82 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class PauseMenu : MonoBehaviour {
+public class PauseMenu : ISingletonScript
+{
+    public enum ClickedAction
+    {
+        Continue,
+        Restart,
+        ReturnToMenu
+    }
 
-	// Use this for initialization
-	void Start () {
-	
+    public GameObject pausePanel;
+    public bool lockCursor = false;
+    System.Action<ClickedAction> onVisibleChanged;
+
+    public override void SingletonStart(Singleton instance)
+    {
+        OnContinueClicked();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    
+    public override void SceneStart(Singleton instance)
+    {
+    }
+
+    public void Show(System.Action<ClickedAction> visibleChanged = null)
+    {
+        // Store function pointer
+        onVisibleChanged = visibleChanged;
+
+        // Unlock the cursor
+        Screen.lockCursor = false;
+
+        // Make the game object active
+        pausePanel.SetActive(true);
+
+        // Stop time
+        Time.timeScale = 0;
+    }
+
+    public void Hide()
+    {
+        OnContinueClicked();
+    }
+
+    public void OnContinueClicked()
+    {
+        OnContinueClicked(ClickedAction.Continue);
+    }
+
+    public void OnRestartClicked()
+    {
+        OnContinueClicked(ClickedAction.Restart);
+        Singleton.Get<SceneTransition>().LoadLevel(Application.loadedLevel);
+    }
+
+    public void OnReturnToMenuClicked()
+    {
+        OnContinueClicked(ClickedAction.ReturnToMenu);
+        Singleton.Get<SceneTransition>().LoadLevel(GameSettings.MenuLevel);
+    }
+
+    void OnContinueClicked(ClickedAction action)
+    {
+        // Make time flow again
+        Time.timeScale = 1;
+
+        // Lock the cursor
+        Screen.lockCursor = lockCursor;
+
+        // Hide the panel
+        pausePanel.SetActive(false);
+
+        // Indicate change
+        if (onVisibleChanged != null)
+        {
+            onVisibleChanged(action);
+            onVisibleChanged = null;
+        }
+    }
 }
