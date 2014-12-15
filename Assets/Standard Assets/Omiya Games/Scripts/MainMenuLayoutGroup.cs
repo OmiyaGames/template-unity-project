3459cb013cc20a73aba8aa498b9b5b4e5ee59c5c
Taro Omiya <taro.omiya@omiyagames.com>
2014-12-15 01:23:42 -0500

    Finally updated the MainMenuLayoutGroup to correctly layout all buttons.

--- Assets/Standard Assets/Omiya Games/Scripts/MainMenuLayoutGroup.cs
+++ Assets/Standard Assets/Omiya Games/Scripts/MainMenuLayoutGroup.cs
@@ -3,270 +3,109 @@ using System.Collections.Generic;
 
 namespace UnityEngine.UI
 {
-    public class MainMenuLayoutGroup : LayoutGroup
+    public class MainMenuLayoutGroup : GridLayoutGroup
     {
-        public enum Corner
-        {
-            UpperLeft = 0,
-            UpperRight,
-            LowerLeft,
-            LowerRight
-        }
-
-        public enum Axis
-        {
-            Horizontal = 0,
-            Vertical
-        }
-
-        public enum Constraint
+        public enum MainMenuConstraint
         {
             FixedColumnCount = 0,
             FixedRowCount
         }
 
         [SerializeField]
-        protected Corner m_StartCorner = Corner.UpperLeft;
-        public Corner startCorner
+        protected MainMenuConstraint m_menuConstraint = MainMenuConstraint.FixedColumnCount;
+        public MainMenuConstraint mainMenuConstraint
         {
             get
             {
-                return m_StartCorner;
+                return m_menuConstraint;
             }
             set
             {
-                SetProperty(ref m_StartCorner, value);
+                SetProperty(ref m_menuConstraint, value);
             }
         }
 
-        [SerializeField]
-        protected Axis m_StartAxis = Axis.Horizontal;
-        public Axis startAxis
-        {
-            get
-            {
-                return m_StartAxis;
-            }
-            set
-            {
-                SetProperty(ref m_StartAxis, value);
-            }
-        }
-
-        [SerializeField]
-        protected Vector2 m_CellSize = new Vector2(100, 100);
-        public Vector2 cellSize
-        {
-            get
-            {
-                return m_CellSize;
-            }
-            set
-            {
-                SetProperty(ref m_CellSize, value);
-            }
-        }
-
-        [SerializeField]
-        protected Vector2 m_Spacing = Vector2.zero;
-        public Vector2 spacing
-        {
-            get
-            {
-                return m_Spacing;
-            }
-            set
-            {
-                SetProperty(ref m_Spacing, value);
-            }
-        }
-
-        [SerializeField]
-        protected Constraint m_Constraint = Constraint.FixedColumnCount;
-        public Constraint constraint
-        {
-            get
-            {
-                return m_Constraint;
-            }
-            set
-            {
-                SetProperty(ref m_Constraint, value);
-            }
-        }
-
-        [SerializeField]
-        protected int m_ConstraintCount = 2;
-        public int constraintCount
-        {
-            get
-            {
-                return m_ConstraintCount;
-            }
-            set
-            {
-                SetProperty(ref m_ConstraintCount, value);
-            }
-        }
+        Vector2 tempCellSize = Vector2.one;
 
         protected MainMenuLayoutGroup()
         { }
 
         public override void CalculateLayoutInputHorizontal()
         {
+            constraint = ConvertToConstraint(mainMenuConstraint);
+            cellSize = CalculateCellSize(mainMenuConstraint, constraintCount);
             base.CalculateLayoutInputHorizontal();
-
-            int minColumns = 0;
-            int preferredColumns = 0;
-            if (m_Constraint == Constraint.FixedColumnCount)
-            {
-                minColumns = preferredColumns = m_ConstraintCount;
-            }
-            else if (m_Constraint == Constraint.FixedRowCount)
-            {
-                minColumns = preferredColumns = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
-            }
-            else
-            {
-                minColumns = 1;
-                preferredColumns = Mathf.CeilToInt(Mathf.Sqrt(rectChildren.Count));
-            }
-
-            SetLayoutInputForAxis(
-                padding.horizontal + (cellSize.x + spacing.x) * minColumns - spacing.x,
-                padding.horizontal + (cellSize.x + spacing.x) * preferredColumns - spacing.x,
-                -1, 0);
         }
 
         public override void CalculateLayoutInputVertical()
         {
-            int minRows = 0;
-            if (m_Constraint == Constraint.FixedColumnCount)
-            {
-                minRows = Mathf.CeilToInt(rectChildren.Count / (float)m_ConstraintCount - 0.001f);
-            }
-            else if (m_Constraint == Constraint.FixedRowCount)
-            {
-                minRows = m_ConstraintCount;
-            }
-            else
-            {
-                float width = rectTransform.rect.size.x;
-                int cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (cellSize.x + spacing.x)));
-                minRows = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX);
-            }
-
-            float minSpace = padding.vertical + (cellSize.y + spacing.y) * minRows - spacing.y;
-            SetLayoutInputForAxis(minSpace, minSpace, -1, 1);
+            constraint = ConvertToConstraint(mainMenuConstraint);
+            cellSize = CalculateCellSize(mainMenuConstraint, constraintCount);
+            base.CalculateLayoutInputVertical();
         }
 
         public override void SetLayoutHorizontal()
         {
-            SetCellsAlongAxis(0);
+            constraint = ConvertToConstraint(mainMenuConstraint);
+            cellSize = CalculateCellSize(mainMenuConstraint, constraintCount);
+            base.SetLayoutHorizontal();
         }
 
         public override void SetLayoutVertical()
         {
-            SetCellsAlongAxis(1);
+            constraint = ConvertToConstraint(mainMenuConstraint);
+            cellSize = CalculateCellSize(mainMenuConstraint, constraintCount);
+            base.SetLayoutVertical();
         }
 
-        private void SetCellsAlongAxis(int axis)
+        Constraint ConvertToConstraint(MainMenuConstraint menuConstraint)
         {
-            // Normally a Layout Controller should only set horizontal values when invoked for the horizontal axis
-            // and only vertical values when invoked for the vertical axis.
-            // However, in this case we set both the horizontal and vertical position when invoked for the vertical axis.
-            // Since we only set the horizontal position and not the size, it shouldn't affect children's layout,
-            // and thus shouldn't break the rule that all horizontal layout must be calculated before all vertical layout.
-
-            if (axis == 0)
+            Constraint returnEnum = Constraint.FixedColumnCount;
+            if(menuConstraint == MainMenuConstraint.FixedRowCount)
             {
-                // Only set the sizes when invoked for horizontal axis, not the positions.
-                for (int i = 0; i < rectChildren.Count; i++)
-                {
-                    RectTransform rect = rectChildren[i];
+                returnEnum = Constraint.FixedRowCount;
+            }
+            return returnEnum;
+        }
 
-                    m_Tracker.Add(this, rect,
-                        DrivenTransformProperties.Anchors |
-                        DrivenTransformProperties.AnchoredPosition |
-                        DrivenTransformProperties.SizeDelta);
-
-                    rect.anchorMin = Vector2.up;
-                    rect.anchorMax = Vector2.up;
-                    rect.sizeDelta = cellSize;
-                }
-                return;
+        Vector2 CalculateCellSize(MainMenuConstraint menuConstraint, int constraintQuantity)
+        {
+            // Calculate the number of columns and rows (assuming menuConstraint is set to default, i.e. FixedColumnCount)
+            int numColumns = Mathf.Max(constraintQuantity, 1);
+            int numRows = Mathf.Max((rectChildren.Count / numColumns), 1);
+            if ((rectChildren.Count % numColumns) > 0)
+            {
+                ++numRows;
             }
 
-            float width = rectTransform.rect.size.x;
-            float height = rectTransform.rect.size.y;
-
-            int cellCountX = 1;
-            int cellCountY = 1;
-            if (m_Constraint == Constraint.FixedColumnCount)
+            // Check if menuConstraint is actually set to default (i.e. FixedColumnCount)
+            if(menuConstraint == MainMenuConstraint.FixedRowCount)
             {
-                cellCountX = m_ConstraintCount;
-                cellCountY = Mathf.CeilToInt(rectChildren.Count / (float)cellCountX - 0.001f);
-            }
-            else if (m_Constraint == Constraint.FixedRowCount)
-            {
-                cellCountY = m_ConstraintCount;
-                cellCountX = Mathf.CeilToInt(rectChildren.Count / (float)cellCountY - 0.001f);
-            }
-            else
-            {
-                cellCountX = Mathf.Max(1, Mathf.FloorToInt((width - padding.horizontal + spacing.x + 0.001f) / (cellSize.x + spacing.x)));
-                cellCountY = Mathf.Max(1, Mathf.FloorToInt((height - padding.vertical + spacing.y + 0.001f) / (cellSize.y + spacing.y)));
+                // If not, swap the values
+                int swap = numColumns;
+                numColumns = numRows;
+                numRows = swap;
             }
 
-            int cornerX = (int)startCorner % 2;
-            int cornerY = (int)startCorner / 2;
+            // Calculate how much space all the buttons cumulatively takes
+            tempCellSize = rectTransform.rect.size;
 
-            int cellsPerMainAxis, actualCellCountX, actualCellCountY;
-            if (startAxis == Axis.Horizontal)
-            {
-                cellsPerMainAxis = cellCountX;
-                actualCellCountX = Mathf.Clamp(cellCountX, 1, rectChildren.Count);
-                actualCellCountY = Mathf.Clamp(cellCountY, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
-            }
-            else
-            {
-                cellsPerMainAxis = cellCountY;
-                actualCellCountY = Mathf.Clamp(cellCountY, 1, rectChildren.Count);
-                actualCellCountX = Mathf.Clamp(cellCountX, 1, Mathf.CeilToInt(rectChildren.Count / (float)cellsPerMainAxis));
-            }
+            // Take out all the padding
+            tempCellSize.x -= padding.left;
+            tempCellSize.x -= padding.right;
+            tempCellSize.y -= padding.top;
+            tempCellSize.y -= padding.bottom;
 
-            Vector2 requiredSpace = new Vector2(
-                    actualCellCountX * cellSize.x + (actualCellCountX - 1) * spacing.x,
-                    actualCellCountY * cellSize.y + (actualCellCountY - 1) * spacing.y
-                    );
-            Vector2 startOffset = new Vector2(
-                    GetStartOffset(0, requiredSpace.x),
-                    GetStartOffset(1, requiredSpace.y)
-                    );
+            // Take out the button spacing
+            tempCellSize.x -= (spacing.x * (numColumns - 1));
+            tempCellSize.y -= (spacing.y * (numRows - 1));
 
-            for (int i = 0; i < rectChildren.Count; i++)
-            {
-                int positionX;
-                int positionY;
-                if (startAxis == Axis.Horizontal)
-                {
-                    positionX = i % cellsPerMainAxis;
-                    positionY = i / cellsPerMainAxis;
-                }
-                else
-                {
-                    positionX = i / cellsPerMainAxis;
-                    positionY = i % cellsPerMainAxis;
-                }
+            // Divide the cumulative space by the number of columns and rows
+            tempCellSize.x /= numColumns;
+            tempCellSize.y /= numRows;
 
-                if (cornerX == 1)
-                    positionX = actualCellCountX - 1 - positionX;
-                if (cornerY == 1)
-                    positionY = actualCellCountY - 1 - positionY;
-
-                SetChildAlongAxis(rectChildren[i], 0, startOffset.x + (cellSize[0] + spacing[0]) * positionX, cellSize[0]);
-                SetChildAlongAxis(rectChildren[i], 1, startOffset.y + (cellSize[1] + spacing[1]) * positionY, cellSize[1]);
-            }
+            // Return the cell size
+            return tempCellSize;
         }
     }
 }
