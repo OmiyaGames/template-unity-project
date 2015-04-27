00559e80a292e23e5b9cdc101b97d405e1203e5f
Taro Omiya <taro.omiya@omiyagames.com>
2015-04-27 17:48:22 -0400

    Organizing the scripts a little more.  Populating Singleton's SceneManager.

---
+++ Assets/Standard Assets/Omiya Games/Scripts/Menus/SplashMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class SplashMenu : MonoBehaviour
+{
+    const string VisibleField = "Visible";
+
+    [SerializeField]
+    bool fadeoutSplashOnStart = true;
+
+	// Use this for initialization
+	IEnumerator Start ()
+    {
+        yield return null;
+        GetComponent<Animator>().SetBool(VisibleField, false);
+	}
+}
