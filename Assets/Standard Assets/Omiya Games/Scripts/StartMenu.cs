30a503793aa2aa1d3c5b9a1ea1ceea34f1fb0b0f
Taro Omiya <taro.omiya@omiyagames.com>
2015-04-27 15:59:58 -0400

    Renaming a bunch of scripts.

--- Assets/Standard Assets/Omiya Games/Scripts/StartMenu.cs
+++ Assets/Standard Assets/Omiya Games/Scripts/StartMenu.cs
@@ -2,7 +2,7 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class MainMenu : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
     [SerializeField]
     MainMenuLayoutGroup levelLayoutGroup;
