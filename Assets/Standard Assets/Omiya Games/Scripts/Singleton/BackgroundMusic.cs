733215222e07192402272332f24bae0dff7b2b31
Taro Omiya <taro.omiya@omiyagames.com>
2015-05-17 16:54:38 -0400

    Fixing the BackgroundMusic, now using an AudioMixer this time.

--- Assets/Standard Assets/Omiya Games/Scripts/Singleton/BackgroundMusic.cs
+++ Assets/Standard Assets/Omiya Games/Scripts/Singleton/BackgroundMusic.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.Audio;
 using System.Collections;
 
 namespace OmiyaGames
@@ -9,22 +10,63 @@ namespace OmiyaGames
     /// </summary>
     public class BackgroundMusic : ISingletonScript
     {
-        [Tooltip("The background music's clip. 2D AudioClip is recommended.")]
-        [SerializeField]
-        AudioClip startingClip = null;
+        [System.Serializable]
+        public class MusicInfo
+        {
+            [SerializeField]
+            AudioSource source = null;
+            [SerializeField]
+            AudioMixerSnapshot snapshot = null;
+
+            public AudioSource Source
+            {
+                get
+                {
+                    return source;
+                }
+            }
+
+            public AudioClip Clip
+            {
+                get
+                {
+                    return source.clip;
+                }
+            }
+
+            public AudioMixerSnapshot Snapshot
+            {
+                get
+                {
+                    return snapshot;
+                }
+            }
+
+            public void ChangeClip(AudioClip clip, float transitionTime)
+            {
+                if(Source.clip != null)
+                {
+                    Source.Stop();
+                }
+                source.clip = clip;
+                if(clip != null)
+                {
+                    Source.Play();
+                }
+                Snapshot.TransitionTo(transitionTime);
+            }
+        }
+
         [Tooltip("The transition length (in seconds) between 2 background musics. Set to -1 if you want no transition.")]
         [SerializeField]
         float transitionDuration = 1;
         [SerializeField]
-        bool loopMusic = true;
-        [Tooltip("The priority of the audio sources when they're created.")]
+        MusicInfo music1 = null;
         [SerializeField]
-        int audioPriority = 128;
+        MusicInfo music2 = null;
 
-        float timePassedInTransition = float.NaN, volumeChangeSpeed = 1;
-        int currentAudioSourceIndex = 0, index = 0;
-        GameSettings settings = null;
-        readonly AudioSource[] allAudioSources = new AudioSource[2];
+        bool isPlayingMusic1 = true;
+        int index = 0;
 
         /// <summary>
         /// Gets or sets the volume of the background music, which is a value between 0 and 1.
@@ -34,31 +76,17 @@ namespace OmiyaGames
         {
             get
             {
-                return Mathf.Clamp01(settings.MusicVolume);
+                return Mathf.Clamp01(Singleton.Get<GameSettings>().MusicVolume);
             }
             set
             {
                 // Set volume
+                GameSettings settings = Singleton.Get<GameSettings>();
                 settings.MusicVolume = Mathf.Clamp01(value);
 
-                // Check if we're transitioning
-                if (float.IsNaN(timePassedInTransition) == true)
-                {
-                    // Update audio sources
-                    CurrentAudioSource.volume = settings.MusicVolume;
-                }
-                else
-                {
-                    // Reduce the volume of each audio source, if it has a louder volume
-                    if (TransitionAudioSource.volume > settings.MusicVolume)
-                    {
-                        TransitionAudioSource.volume = settings.MusicVolume;
-                    }
-                    if (CurrentAudioSource.volume > settings.MusicVolume)
-                    {
-                        CurrentAudioSource.volume = settings.MusicVolume;
-                    }
-                }
+                // Update audio sources
+                music1.Source.volume = settings.MusicVolume;
+                music2.Source.volume = settings.MusicVolume;
             }
         }
 
@@ -66,11 +94,17 @@ namespace OmiyaGames
         {
             get
             {
-                return settings.IsMusicMuted;
+                return Singleton.Get<GameSettings>().IsMusicMuted;
             }
             set
             {
+                // Set mute
+                GameSettings settings = Singleton.Get<GameSettings>();
                 settings.IsMusicMuted = value;
+
+                // Update audio sources
+                music1.Source.mute = value;
+                music2.Source.mute = value;
             }
         }
 
@@ -78,152 +112,63 @@ namespace OmiyaGames
         {
             get
             {
-                return CurrentAudioSource.clip;
+                return CurrentAudioSource.Clip;
             }
             set
             {
                 // Check if this is a different clip
-                if (CurrentAudioSource.clip != value)
+                if (CurrentAudioSource.Clip != value)
                 {
-                    // Check if we want to set the background music immediately
-                    if (float.IsNaN(volumeChangeSpeed) == true)
+                    // Swap to the next audio source
+                    isPlayingMusic1 = !isPlayingMusic1;
+                    if(isPlayingMusic1 == true)
                     {
-                        // If not, just set the current audio source to the new clip
-                        CurrentAudioSource.clip = value;
+                        music1.ChangeClip(value, transitionDuration);
                     }
                     else
                     {
-                        // Otherwise, switch to the next audio source
-                        currentAudioSourceIndex += 1;
-
-                        // Setup the next audio source
-                        CurrentAudioSource.Stop();
-                        CurrentAudioSource.clip = value;
-                        CurrentAudioSource.volume = 0;
-
-                        // Start playing the new audio source
-                        CurrentAudioSource.Play();
-
-                        // Flag we're transitioning
-                        timePassedInTransition = 0;
+                        music2.ChangeClip(value, transitionDuration);
                     }
                 }
             }
         }
 
-        AudioSource CurrentAudioSource
+        MusicInfo CurrentAudioSource
         {
             get
             {
-                return allAudioSources[currentAudioSourceIndex];
+                if(isPlayingMusic1 == true)
+                {
+                    return music1;
+                }
+                else
+                {
+                    return music2;
+                }
             }
         }
 
-        AudioSource TransitionAudioSource
+        MusicInfo TransitionAudioSource
         {
             get
             {
-                int nextSource = currentAudioSourceIndex - 1;
-                if (nextSource < 0)
+                if(isPlayingMusic1 == true)
                 {
-                    nextSource = allAudioSources.Length - 1;
+                    return music2;
                 }
-                return allAudioSources[nextSource];
+                else
+                {
+                    return music1;
+                }
             }
         }
 
         public override void SingletonAwake(Singleton instance)
         {
-            // Do nothing
         }
 
         public override void SceneAwake(Singleton instance)
         {
-            // Don't do anything if this object is already setup
-            if (settings != null)
-            {
-                return;
-            }
-
-            // Grab an instance of the settings
-            settings = Singleton.Get<GameSettings>();
-
-            // Setup variables
-            timePassedInTransition = float.NaN;
-            currentAudioSourceIndex = 0;
-
-            // Calculate the volume change speed
-            if ((transitionDuration < 0) || (Mathf.Approximately(transitionDuration, 0) == true))
-            {
-                volumeChangeSpeed = float.NaN;
-            }
-            else
-            {
-                volumeChangeSpeed = 1f / transitionDuration;
-            }
-
-            // Go through all audio sources
-            for (index = 0; index < allAudioSources.Length; ++index)
-            {
-                // Create audio sources
-                allAudioSources[index] = gameObject.AddComponent<AudioSource>();
-
-                // Update this audio's settings
-                allAudioSources[index].spatialBlend = 0f;
-                allAudioSources[index].mute = IsMuted;
-                allAudioSources[index].volume = Volume;
-                allAudioSources[index].priority = audioPriority;
-                allAudioSources[index].loop = loopMusic;
-
-                // Check if we should play this audio
-                if (index == currentAudioSourceIndex)
-                {
-                    allAudioSources[index].playOnAwake = true;
-                    allAudioSources[index].clip = startingClip;
-                    allAudioSources[index].Play();
-                }
-                else
-                {
-                    allAudioSources[index].playOnAwake = false;
-                    allAudioSources[index].Stop();
-                }
-            }
-
-            // Bind to the singleton's update event
-            if (instance != null)
-            {
-                instance.OnRealTimeUpdate += RealTimeUpdate;
-            }
-        }
-
-        void RealTimeUpdate(float deltaTime)
-        {
-            // Check the timePassedInTransition
-            if (float.IsNaN(timePassedInTransition) == false)
-            {
-                // Check if enough time has passed
-                if (timePassedInTransition > transitionDuration)
-                {
-                    // Stop the transition music
-                    TransitionAudioSource.Stop();
-
-                    // Snap the volume of the current music
-                    CurrentAudioSource.volume = Volume;
-
-                    // Flag we're done transitioning
-                    timePassedInTransition = float.NaN;
-                }
-                else
-                {
-                    // Reduce the transition audio's volume to 0
-                    TransitionAudioSource.volume = Mathf.MoveTowards(
-                        TransitionAudioSource.volume, 0, (volumeChangeSpeed * deltaTime));
-
-                    // Increase the current audio's volume to Volume
-                    CurrentAudioSource.volume = Mathf.MoveTowards(
-                        CurrentAudioSource.volume, Volume, (volumeChangeSpeed * deltaTime));
-                }
-            }
         }
     }
 }
