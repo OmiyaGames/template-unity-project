411a7dbf589aa4e2b2607992f2304b3c8c98b69c
Taro Omiya <taro.omiya@omiyagames.com>
2015-08-18 17:57:23 -0400

    Merging GameSettings.cs from Not a Clone project to GameSettings.cs.

--- Assets/Standard Assets/Omiya Games/Scripts/Singleton/GameSettings.cs
+++ Assets/Standard Assets/Omiya Games/Scripts/Singleton/GameSettings.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Collections;
 
 namespace OmiyaGames
 {
@@ -28,7 +27,7 @@ namespace OmiyaGames
     /// THE SOFTWARE.
     /// </copyright>
     /// <author>Taro Omiya</author>
-    /// <date>5/18/2015</date>
+    /// <date>8/18/2015</date>
     ///-----------------------------------------------------------------------
     /// <summary>
     /// A singleton script to retrieve settings stored in <code>PlayerPrefs</code>.
@@ -52,27 +51,25 @@ namespace OmiyaGames
         /// </summary>
         public const int AppVersion = 0;
 
-        public const int BeginnerBestScoreThreshold = 10;
-        public const int NumberOfBeginnerGames = 5;
+        public const int DefaultNumLevelsUnlocked = 1;
 
         public const float DefaultMusicVolume = 1;
         public const float DefaultSoundVolume = 1;
-        public const int DefaultBestScore = 0;
         public const string DefaultLanguage = "";
 
         public const string VersionKey = "AppVersion";
+        public const string NumLevelsUnlockedKey = "Number of Unlocked Levels";
         public const string MusicVolumeKey = "Music Volume";
         public const string MusicMutedKey = "Music Muted";
         public const string SoundVolumeKey = "Sound Volume";
         public const string SoundMutedKey = "Sound Muted";
-        public const string BestScoreKey = "PersonalBest";
         public const string LanguageKey = "Language";
 
         [SerializeField]
         bool simulateWebplayer = false;
 
-        int bestScore = DefaultBestScore;
-        float musicVolume = DefaultMusicVolume, soundVolume = DefaultSoundVolume;
+        int numLevelsUnlocked = 1;
+        float musicVolume = 0, soundVolume = 0;
         bool musicMuted = false, soundMuted = false;
         AppStatus status = AppStatus.Replaying;
         string language = DefaultLanguage;
@@ -107,6 +104,18 @@ namespace OmiyaGames
             get
             {
                 return status;
+            }
+        }
+
+        public int NumLevelsUnlocked
+        {
+            get
+            {
+                return numLevelsUnlocked;
+            }
+            internal set
+            {
+                PlayerPrefs.SetInt(NumLevelsUnlockedKey, NumLevelsUnlocked);
             }
         }
 
@@ -162,22 +171,6 @@ namespace OmiyaGames
             }
         }
 
-        public int BestScore
-        {
-            get
-            {
-                return bestScore;
-            }
-            set
-            {
-                if(bestScore < value)
-                {
-                    bestScore = value;
-                    PlayerPrefs.SetInt(BestScoreKey, bestScore);
-                }
-            }
-        }
-
         public string Language
         {
             get
@@ -188,14 +181,6 @@ namespace OmiyaGames
             {
                 language = value;
                 PlayerPrefs.SetString(LanguageKey, language);
-            }
-        }
-
-        public bool IsBeginnerModeOn
-        {
-            get
-            {
-                return (BestScore < BeginnerBestScoreThreshold);
             }
         }
         #endregion
@@ -222,17 +207,20 @@ namespace OmiyaGames
 
             // Update the app status
             status = AppStatus.Replaying;
-            if(currentVersion < 0)
+            if (currentVersion < 0)
             {
                 status = AppStatus.FirstTimeOpened;
             }
-            else if(currentVersion < AppVersion)
+            else if (currentVersion < AppVersion)
             {
                 status = AppStatus.RecentlyUpdated;
             }
 
             // Set the version
             PlayerPrefs.SetInt(VersionKey, AppVersion);
+
+            // Grab the number of levels unlocked
+            numLevelsUnlocked = PlayerPrefs.GetInt(NumLevelsUnlockedKey, DefaultNumLevelsUnlocked);
 
             // Grab the music settings
             musicVolume = PlayerPrefs.GetFloat(MusicVolumeKey, DefaultMusicVolume);
@@ -242,15 +230,18 @@ namespace OmiyaGames
             soundVolume = PlayerPrefs.GetFloat(SoundVolumeKey, DefaultSoundVolume);
             soundMuted = (PlayerPrefs.GetInt(SoundMutedKey, 0) != 0);
 
-            // Grab the best score
-            bestScore = PlayerPrefs.GetInt(BestScoreKey, DefaultBestScore);
-
             // Grab the language
             language = PlayerPrefs.GetString(LanguageKey, DefaultLanguage);
+
+            // NOTE: Feel free to add more stuff here
+
         }
 
         public void SaveSettings()
         {
+            // Save the number of levels unlocked
+            PlayerPrefs.SetInt(NumLevelsUnlockedKey, NumLevelsUnlocked);
+
             // Save the music settings
             PlayerPrefs.SetFloat(MusicVolumeKey, musicVolume);
             PlayerPrefs.SetInt(MusicMutedKey, (musicMuted ? 1 : 0));
@@ -259,11 +250,10 @@ namespace OmiyaGames
             PlayerPrefs.SetFloat(SoundVolumeKey, soundVolume);
             PlayerPrefs.SetInt(SoundMutedKey, (soundMuted ? 1 : 0));
 
-            // Set the best score
-            PlayerPrefs.SetInt(BestScoreKey, bestScore);
-
             // Set the language
             PlayerPrefs.SetString(LanguageKey, language);
+
+            // NOTE: Feel free to add more stuff here
 
             PlayerPrefs.Save();
         }
