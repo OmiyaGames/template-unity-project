14b6090dbafa3c91657fab370b8a184301215e5f
Taro Omiya <taro.omiya@omiyagames.com>
2016-04-14 18:38:57 -0400

    Adding licensing to SocialManager

--- Assets/Omiya Games/Scripts/Singleton/SocialManager.cs
+++ Assets/Omiya Games/Scripts/Singleton/SocialManager.cs
@@ -11,6 +11,38 @@ using GooglePlayGames.BasicApi;
 
 namespace OmiyaGames
 {
+    ///-----------------------------------------------------------------------
+    /// <copyright file="SocialManagerWrapperClasses.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2015 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>4/14/2016</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// Singleton file used to authenticate with various social platforms.
+    /// <code>Singleton</code>'s events.
+    /// </summary>
+    /// <seealso cref="UnityEngine.SocialPlatforms"/>
     public partial class SocialManager : ISingletonScript
     {
         public enum LogInState
