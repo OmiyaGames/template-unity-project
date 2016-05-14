a2c231c158ef1cef9a0ece7b3041d003c697337c
Taro Omiya <taro.omiya@omiyagames.com>
2016-05-14 11:06:24 -0400

    WARNING: untested (probably non-compiling) commit! Working on creating a ScriptableObject asset from DomainListAssetBundleGenerator. Will work on generating AssetBundle from this file next.

--- Assets/Omiya Games/Editor/DomainListAssetBundleGenerator.cs
+++ Assets/Omiya Games/Editor/DomainListAssetBundleGenerator.cs
@@ -3,12 +3,14 @@ using UnityEditor;
 using UnityEditorInternal;
 using System.Collections.Generic;
 using System;
+using System.Text;
 
 public class DomainListAssetBundleGenerator : EditorWindow
 {
     const float VerticalMargin = 2;
 
     string nameOfFile = "domains";
+    string nameOfFolder = "Assets";
     List<string> allDomains = new List<string> { "localhost", "build.cloud.unity3d.com" };
     ReorderableList allDomainsField = null;
 
@@ -22,10 +24,14 @@ public class DomainListAssetBundleGenerator : EditorWindow
     void OnGUI()
     {
         // Explain what this dialog does
-        EditorGUILayout.HelpBox("What is a Domain List Asset?\nIt's an asset containing a list of domains the WebLocationChecker script uses to check if the domain of the website the WebGL build is running in is an accepted host or not. Using this method vs. the normal text-based method makes the asset serialized, and as such, harder to change.", MessageType.Info);
+        EditorGUILayout.HelpBox("What is a Domain List Asset?\nIt's an asset containing a list of domains the WebLocationChecker script uses to check if the domain of the website the WebGL build is running in is an accepted host or not. Since Asset Bundles are serialized, and thus, harder to edit, it creates a more secure method for developers to change the list of approved domains on-the-fly while making it harder for others to change it.", MessageType.Info);
 
         // Ask the list of layouts
         allDomainsField.DoLayoutList();
+
+        // Ask the path this will generate
+        GUILayout.Label("Name of folder to generate this asset");
+        nameOfFolder = GUILayout.TextField(nameOfFolder);
 
         // Ask the name of the file this will generate
         GUILayout.Label("Name of asset to generate");
@@ -34,6 +40,33 @@ public class DomainListAssetBundleGenerator : EditorWindow
         // Generate the asset
         if (GUILayout.Button("Generate Domain Asset") == true)
         {
+            // Generate a path to create this asset
+            StringBuilder builder = new StringBuilder();
+            builder.Append(nameOfFolder);
+            if(nameOfFolder.EndsWith("/") == false)
+            {
+                builder.Append('/');
+            }
+            builder.Append(nameOfFile);
+            if(nameOfFolder.EndsWith(".asset") == false)
+            {
+                builder.Append(".asset");
+            }
+            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath (builder.ToString());
+            
+            // FIXME: remove this ScriptableObject generation, and just create an asset bundle straight from it
+            // Create the asset at the assigned path
+            AcceptedDomainList domainList = ScriptableObject.CreateInstance<AcceptedDomainList>();
+            domainList.name = nameOfFile;
+            AssetDatabase.CreateAsset (domainList, assetPathAndName);
+            AssetDatabase.SaveAssets ();
+            
+            // Select this asset
+            AssetDatabase.Refresh();
+            EditorUtility.FocusProjectWindow ();
+            Selection.activeObject = domainList;
+            
+            //FIXME: create an AssetBundle from this asset
             // FIXME: do something!
             Debug.Log("You pushed the button!");
             foreach(string domain in allDomains)
