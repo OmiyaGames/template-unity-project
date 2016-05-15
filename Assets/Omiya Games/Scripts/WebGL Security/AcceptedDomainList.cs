d8567f49122d7640cda74de1d364e8d4ab748464
TVGS <info@techvalleygamespace.com>
2016-05-14 20:45:34 -0400

    Updating license to the new files.

--- Assets/Omiya Games/Scripts/WebGL Security/AcceptedDomainList.cs
+++ Assets/Omiya Games/Scripts/WebGL Security/AcceptedDomainList.cs
@@ -2,6 +2,39 @@
 
 namespace OmiyaGames
 {
+    ///-----------------------------------------------------------------------
+    /// <copyright file="AcceptedDomainList.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2016 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <date>5/14/2016</date>
+    /// <author>Taro Omiya</author>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// <code>ScriptableObject</code> that contains a list of strings. Used to
+    /// create a list of domains the <code>WebLocationChecker</code> can download.
+    /// </summary>
+    /// <seealso cref="DomainListAssetBundleGenerator"/>
+    /// <seealso cref="WebLocationChecker"/>
     public class AcceptedDomainList : ScriptableObject
     {
         [SerializeField]
