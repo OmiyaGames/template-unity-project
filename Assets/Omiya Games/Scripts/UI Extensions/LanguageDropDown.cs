097d944e9b8c34e5b1685cd5e4391399bcc750cc
Taro Omiya <taro.omiya@omiyagames.com>
2018-06-05 14:15:36 -0400

    Fixing comments and formatting.  Adding some extra helper context menu functions.

--- Assets/Omiya Games/Scripts/UI Extensions/LanguageDropDown.cs
+++ Assets/Omiya Games/Scripts/UI Extensions/LanguageDropDown.cs
@@ -2,25 +2,70 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 namespace OmiyaGames
 {
+    ///-----------------------------------------------------------------------
+    /// <copyright file="OmiyaGamesUtility.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2018 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>4/2/2017</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// A drop-down for languages
+    /// </summary>
+    /// <remarks>
+    /// Revision History:
+    /// <list type="table">
+    /// <listheader>
+    /// <description>Date</description>
+    /// <description>Name</description>
+    /// <description>Description</description>
+    /// </listheader>
+    /// <item>
+    /// <description>4/2/2017</description>
+    /// <description>Taro</description>
+    /// <description>Initial verison</description>
+    /// </item>
+    /// </list>
+    // FIXME: Replace Dropdown with Text Mesh Pro equivalent
+    [DisallowMultipleComponent]
     public class LanguageDropDown : Dropdown
     {
         readonly Dictionary<DropdownItem, Text> allDropDownItems = new  Dictionary<DropdownItem, Text>();
-        bool isSetup = false, isItemsSetup = false;
+        bool isItemsSetup = false;
 
         public bool IsSetup
         {
-            get
-            {
-                return isSetup;
-            }
-        }
-        
+            get;
+            private set;
+        } = false;
+
         public void Setup()
         {
-            if(isSetup == false)
+            if(IsSetup == false)
             {
                 // Remove all language options
                 ClearOptions();
@@ -34,7 +79,7 @@ namespace OmiyaGames
                 onValueChanged.AddListener(UpdateCaptionFont);
 
                 // Indicate we're setup
-                isSetup = true;
+                IsSetup = true;
             }
         }
 
