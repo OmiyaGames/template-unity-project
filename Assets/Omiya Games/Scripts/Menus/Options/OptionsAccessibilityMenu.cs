d9e5500ec06ef5bab5fffd50c609ad019600d373
Taro Omiya <taro.omiya@omiyagames.com>
2018-06-17 20:27:23 -0400

    More work on setting up the accessibility options menu.

--- Assets/Omiya Games/Scripts/Menus/Options/OptionsAccessibilityMenu.cs
+++ Assets/Omiya Games/Scripts/Menus/Options/OptionsAccessibilityMenu.cs
@@ -53,11 +53,15 @@ namespace OmiyaGames.Menu
 
         [Header("Text Size Controls")]
         [SerializeField]
+        float defaultTextSize = 1f;
+        [SerializeField]
         Slider textSizeSlider;
         [SerializeField]
         Button resetTextSize;
 
         [Header("Time Scale Controls")]
+        [SerializeField]
+        float defaultTimeScale = 1f;
         [SerializeField]
         AudioVolumeControls timeScaleSlider;
         [SerializeField]
@@ -78,11 +82,11 @@ namespace OmiyaGames.Menu
             get
             {
                 GameObject returnUi = null;
-                if(enableTextSize.IsThisBuildSupported() == true)
+                if (enableTextSize.IsThisBuildSupported() == true)
                 {
                     returnUi = textSizeSlider.gameObject;
                 }
-                else if(enableTimeScale.IsThisBuildSupported() == true)
+                else if (enableTimeScale.IsThisBuildSupported() == true)
                 {
                     returnUi = timeScaleSlider.Checkbox.gameObject;
                 }
@@ -96,13 +100,103 @@ namespace OmiyaGames.Menu
             // Call base method
             base.OnSetup();
 
-            // FIXME: setup other controls
+            // FIXME: setup sliders
+            //timeScaleSlider.Setup();
+
+            // Setup reset buttons
+            // FIXME: fix these arguments to match the settings.
+            //UpdateResetTimeScaleButton(false, defaultTimeScale);
+            //UpdateResetTextSizeButton(defaultTextSize);
+
+            // Setup control events
+            timeScaleSlider.OnCheckboxUpdated += TimeScaleSlider_OnCheckboxUpdated;
+            timeScaleSlider.OnSliderValueUpdated += TimeScaleSlider_OnSliderValueUpdated;
+            timeScaleSlider.OnSliderReleaseUpdated += TimeScaleSlider_OnSliderValueUpdated;
+
+            // Setup dividers
+            OptionsMenuUtility.SetupDividers(dividers, enableTextSize, enableTimeScale);
         }
 
         #region UI events
+        public void OnResetTextSizeClicked()
+        {
+            if (IsListeningToEvents == true)
+            {
+                // Reset the text size slider to default
+                textSizeSlider.value = defaultTextSize;
+            }
+        }
+
+        public void OnResetTimeScaleClicked()
+        {
+            if (IsListeningToEvents == true)
+            {
+                // Reset the time scale slider to default
+                timeScaleSlider.Slider.value = defaultTimeScale;
+                timeScaleSlider.Checkbox.isOn = false;
+            }
+        }
+
+        public void OnTextSizeSliderChanged(float percent)
+        {
+            if (IsListeningToEvents == true)
+            {
+                // Check slider value
+                if (Mathf.Approximately(percent, defaultTextSize) == false)
+                {
+                    // FIXME: Set the text size on TextSizeResizer
+                }
+                else
+                {
+                    // FIXME: Set the text size on TextSizeResizer to default
+                }
+
+                // Check slider value
+                UpdateResetTextSizeButton(percent);
+            }
+        }
+
+        void TimeScaleSlider_OnCheckboxUpdated(bool isChecked)
+        {
+            if (IsListeningToEvents == true)
+            {
+                // Update the reset time scale button
+                UpdateResetTimeScaleButton(isChecked, timeScaleSlider.Slider.value);
+            }
+        }
+
+        void TimeScaleSlider_OnSliderValueUpdated(float percent)
+        {
+            if (IsListeningToEvents == true)
+            {
+                // Check slider value
+                if (Mathf.Approximately(percent, defaultTimeScale) == false)
+                {
+                    // FIXME: Set the time scale on time manager to slider
+                }
+                else
+                {
+                    // FIXME: Set the time scale on time manager to default
+                }
+
+                // Update the reset time scale button
+                UpdateResetTimeScaleButton(timeScaleSlider.Checkbox.isOn, percent);
+            }
+        }
         #endregion
 
         #region Helper Methods
+        void UpdateResetTimeScaleButton(bool isChecked, float percent)
+        {
+            // Check if this checkbox is checked, and the slider is of a different value from default
+            resetTimeScale.interactable = ((isChecked == true) && (Mathf.Approximately(percent, defaultTextSize) == false));
+        }
+
+        void UpdateResetTextSizeButton(float percent)
+        {
+            // Check if the slider is of a different value from default
+            resetTextSize.interactable = (Mathf.Approximately(percent, defaultTextSize) == false);
+        }
         #endregion
     }
 }
