890d88d4ff297ca5369523e363ee456279d8f405
Taro Omiya <taro.omiya@omiyagames.com>
2018-06-26 21:36:09 -0400

    Working on setting up the LevelIntroMenu that adopts to platform and whether the mouse cursor is locked or not.

---
+++ Assets/Omiya Games/Scripts/Attributes/DefaultObjectAttribute.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System;
+
+namespace OmiyaGames
+{
+    ///-----------------------------------------------------------------------
+    /// <copyright file="DefaultObjectAttribute.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2018 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>6/26/2018</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// Creates a checkbox in the editor.
+    /// If unchecked, this value is set to null.
+    /// If checked, reveals an object field, allowing the user to change this value.
+    /// </summary>
+    /// <remarks>
+    /// Revision History:
+    /// <list type="table">
+    /// <listheader>
+    /// <description>Date</description>
+    /// <description>Name</description>
+    /// <description>Description</description>
+    /// </listheader>
+    /// <item>
+    /// <description>6/26/2018</description>
+    /// <description>Taro</description>
+    /// <description>Initial verison</description>
+    /// </item>
+    /// </list>
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class DefaultObjectAttribute : PropertyAttribute
+    {
+        /// <summary>
+        /// Creates a checkbox in the editor.
+        /// If unchecked, this value is set to null.
+        /// If checked, reveals an object field, allowing the user to change this value.
+        /// </summary>
+        public DefaultObjectAttribute()
+        {
+        }
+    }
+}
