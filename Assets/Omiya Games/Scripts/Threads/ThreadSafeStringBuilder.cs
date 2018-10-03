d88ea511bf9e5e1180675eb62808d504c318f0b6
Taro Omiya <taro.omiya@omiyagames.com>
2018-10-02 20:50:54 -0400

    Adding ThreadSafeStringBuilder.

---
+++ Assets/Omiya Games/Scripts/Threads/ThreadSafeStringBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace OmiyaGames
+{
+    public class ThreadSafeStringBuilder : ThreadSafe<StringBuilder>
+    {
+        public override string ToString()
+        {
+            lock (ThreadLock)
+            {
+                return value.ToString();
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                lock (ThreadLock)
+                {
+                    return value.Length;
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            lock (ThreadLock)
+            {
+                value.Clear();
+            }
+        }
+
+        public void Append(string append)
+        {
+            lock (ThreadLock)
+            {
+                value.Append(append);
+            }
+        }
+
+        public void AppendLine()
+        {
+            lock (ThreadLock)
+            {
+                value.AppendLine();
+            }
+        }
+    }
+}
