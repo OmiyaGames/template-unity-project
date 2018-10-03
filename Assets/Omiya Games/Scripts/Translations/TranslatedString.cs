63c0adc77edb49dfb5ad352e92ee35788659177d
Taro Omiya <taro.omiya@omiyagames.com>
2018-10-03 14:48:02 -0400

    Updating TranslatedString to utilize the TranslationDictionary.  Starting a bit complex with events tracking lanugage changes and all that, but I'm sort of regretting adding that unnecessary complexity.

--- Assets/Omiya Games/Scripts/Translations/TranslatedString.cs
+++ Assets/Omiya Games/Scripts/Translations/TranslatedString.cs
@@ -28,26 +28,61 @@ namespace OmiyaGames.Translations
     /// THE SOFTWARE.
     /// </copyright>
     /// <author>Taro Omiya</author>
-    /// <date>3/23/2017</date>
+    /// <date>10/3/2018</date>
     ///-----------------------------------------------------------------------
     /// <summary>
     /// A struct whose <code>ToString()</code> method automatically translates
     /// based on settings.
     /// </summary>
+    /// <remarks>
+    /// Revision History:
+    /// <list type="table">
+    /// <listheader>
+    /// <description>Date</description>
+    /// <description>Name</description>
+    /// <description>Description</description>
+    /// </listheader>
+    /// <item>
+    /// <description>3/23/2017</description>
+    /// <description>Taro Omiya</description>
+    /// <description>Initial verison</description>
+    /// 
+    /// <description>10/3/2018</description>
+    /// <description>Taro</description>
+    /// <description>Upgrading with TranslationDictionary support</description>
+    /// </item>
+    /// </list>
+    /// </remarks>
     [Serializable]
-    public class TranslatedString
+    public class TranslatedString : IDisposable
     {
         [SerializeField]
-        readonly string key;
+        string key;
+        [SerializeField]
+        TranslationDictionary dictionary;
 
-        public TranslatedString(string key) : this(key, null)
-        {
-        }
+        object[] values;
+        TranslationManager.LanguageChanged onLanguageChanged = null;
+        /// <summary>
+        /// If null, this string is marked dirty.
+        /// </summary>
+        string translatedFormattedText = null;
+
+        public TranslatedString(string key) : this(key, null) { }
 
         public TranslatedString(string key, params object[] values)
         {
             this.key = key;
-            Values = values;
+            this.values = values;
+        }
+
+        #region Properties
+        private static TranslationManager Manager
+        {
+            get
+            {
+                return Singleton.Get<TranslationManager>();
+            }
         }
 
         public string TranslationKey
@@ -56,48 +91,180 @@ namespace OmiyaGames.Translations
             {
                 return key;
             }
+            set
+            {
+                // Check if the value is actually different
+                if(key != value)
+                {
+                    // Set key
+                    key = value;
+
+                    // Mark dirty
+                    SetDirty();
+                }
+            }
         }
 
         public object[] Values
         {
-            get;
-            set;
+            get
+            {
+                return values;
+            }
+            set
+            {
+                // Set the values directly; it's not performant to check whether all objects are different
+                values = value;
+
+                // Mark dirty
+                SetDirty();
+            }
+        }
+
+        public TranslationDictionary Dictionary
+        {
+            get
+            {
+                return dictionary;
+            }
+            set
+            {
+                // Check if the value is different
+                if (dictionary != value)
+                {
+                    // Set dictionary
+                    dictionary = value;
+
+                    // Mark dirty
+                    SetDirty();
+                }
+            }
+        }
+
+        public bool IsDirty
+        {
+            get
+            {
+                return (translatedFormattedText == null);
+            }
         }
 
         public bool IsTranslating
         {
             get
             {
-                return (string.IsNullOrEmpty(TranslationKey) == false) && (Parser != null) && (Parser.ContainsKey(TranslationKey) == true);
+                return (string.IsNullOrEmpty(TranslationKey) == false) && (dictionary != null) && (dictionary.AllTranslations.ContainsKey(TranslationKey) == true);
             }
         }
+        #endregion
 
-        private static TranslationManager Parser
-        {
-            get
-            {
-                return Singleton.Get<TranslationManager>();
-            }
-        }
-
+        /// <summary>
+        /// Generates a translated text based on GameSettings
+        /// </summary>
+        /// <returns>A translated text.</returns>
         public override string ToString()
         {
-            string returnString = base.ToString();
+            // Check if we can translate
             if (IsTranslating == true)
             {
-                // Add this script to the dictionary
-                returnString = Parser[TranslationKey];
-                if((Values != null) && (Values.Length > 0))
+                // Check whether TranslationManager is available, and we need to generate a new string
+                if ((IsDirty != true) && (Manager != null))
                 {
-                    returnString = string.Format(returnString, Values);
+                    // Check if we've binded to language change event
+                    if (onLanguageChanged == null)
+                    {
+                        // If not, bind to the language manager, setting the flag dirty on language change
+                        onLanguageChanged = new TranslationManager.LanguageChanged(SetDirty);
+                        Manager.OnAfterLanguageChanged += onLanguageChanged;
+                    }
+
+                    // Retrieve the language
+                    translatedFormattedText = ToString(Manager.CurrentLanguage);
+                }
+                else if (Manager == null)
+                {
+                    Dispose();
                 }
             }
+            return translatedFormattedText;
+        }
+
+        /// <summary>
+        /// Generates a translated text based on input language
+        /// </summary>
+        /// <remarks>
+        /// A new string will be generated each time,
+        /// making this operation potentially slow.
+        /// </remarks>
+        /// <returns></returns>
+        public string ToString(int languageIndex)
+        {
+            string returnString = null;
+            if ((IsTranslating == true) && (dictionary.SupportedLanguages.Contains(languageIndex) == true))
+            {
+                returnString = AddFormatting(dictionary[TranslationKey, languageIndex]);
+            }
             return returnString;
+        }
+
+        /// <summary>
+        /// Generates a translated text based on input language
+        /// </summary>
+        /// <remarks>
+        /// A new string will be generated each time,
+        /// making this operation potentially slow.
+        /// </remarks>
+        /// <returns></returns>
+        public string ToString(string language)
+        {
+            string returnString = null;
+            if ((IsTranslating == true) && (dictionary.SupportedLanguages.Contains(language) == true))
+            {
+                returnString = AddFormatting(dictionary[TranslationKey, language]);
+            }
+            return returnString;
+        }
+
+        /// <summary>
+        /// Cleans-up events and generated string.
+        /// </summary>
+        public void Dispose()
+        {
+            if (onLanguageChanged != null)
+            {
+                // Unbind to the language change event
+                Manager.OnAfterLanguageChanged -= onLanguageChanged;
+                onLanguageChanged = null;
+            }
+
+            // Dispose the generated string
+            SetDirty();
         }
 
         public void SetValues(params object[] values)
         {
             Values = values;
         }
+
+        #region Helper Methods
+        private string AddFormatting(string translatedText)
+        {
+            if ((Values != null) && (Values.Length > 0))
+            {
+                translatedText = string.Format(translatedText, Values);
+            }
+            return translatedText;
+        }
+
+        private void SetDirty()
+        {
+            translatedFormattedText = null;
+        }
+
+        private void SetDirty(TranslationManager source, string lastLanguage, string currentLanguage)
+        {
+            SetDirty();
+        }
+        #endregion
     }
 }
