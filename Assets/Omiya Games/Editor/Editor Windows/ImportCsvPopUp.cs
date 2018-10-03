23845fa1e43afc07b5b7e2fad31204c799cd61ec
Taro Omiya <taro.omiya@omiyagames.com>
2018-10-02 20:51:30 -0400

    Updating ImportCsvPopUps to utilize the new CSVReader's method.  Tracking progress is next.

--- Assets/Omiya Games/Editor/Editor Windows/ImportCsvPopUp.cs
+++ Assets/Omiya Games/Editor/Editor Windows/ImportCsvPopUp.cs
@@ -1,9 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using UnityEditorInternal;
-using System.Collections.Generic;
 using System.Text;
-using System.IO;
+using System.Threading;
+using System.Collections.Generic;
 using OmiyaGames.Translations;
 
 namespace OmiyaGames.UI.Translations
@@ -38,7 +37,7 @@ namespace OmiyaGames.UI.Translations
     /// <summary>
     /// Creates a window that imports CSV files.
     /// </summary>
-    public class ImportCsvPopUp : EditorWindow
+    public partial class ImportCsvPopUp : EditorWindow
     {
         public enum ConflictResolution
         {
@@ -47,6 +46,7 @@ namespace OmiyaGames.UI.Translations
             Overwrite
         }
 
+        #region Constants
         const float ImportButtonWidth = 50f;
         const float SideMargin = 6f;
         const float Space = 4f;
@@ -67,7 +67,9 @@ namespace OmiyaGames.UI.Translations
             (int)ConflictResolution.AppendOverwrite,
             (int)ConflictResolution.Overwrite
         };
-        static readonly Vector2 DefaultWindowSize = new Vector2(350f, 100f);
+        static readonly Vector2 DefaultWindowSize = new Vector2(350f, 150f);
+        #endregion
+
         static GUIStyle browseButtonFont = null;
 
         public static void ShowPopUp(TranslationDictionary dictionaryToEdit)
@@ -77,6 +79,9 @@ namespace OmiyaGames.UI.Translations
             window.minSize = DefaultWindowSize;
             window.Show();
         }
+
+        readonly ThreadSafe<float> progress = new ThreadSafe<float>(-1f);
+        readonly CSVReader.ReadStatus csvReadStatus = new CSVReader.ReadStatus();
 
         #region Properties
         TranslationDictionary DictionaryToEdit
@@ -91,50 +96,46 @@ namespace OmiyaGames.UI.Translations
             set;
         } = null;
 
+        string KeysColumnName
+        {
+            get;
+            set;
+        } = "Keys";
+
         ConflictResolution Resolution
         {
             get;
             set;
         } = ConflictResolution.AppendIgnore;
 
-        float Progress
-        {
-            get;
-            set;
-        } = -1f;
-
         StringBuilder ErrorMessage
         {
             get;
         } = new StringBuilder();
-        #endregion
 
-        #region Unity Events
-        void OnGUI()
+        float Progress
         {
-            ErrorMessage.Clear();
-            GUI.enabled = (Progress < 0);
-            EditorGUILayout.BeginVertical(GUILayout.ExpandHeight(true));
-            EditorGUILayout.Space();
-
-            // Draw the file we'll be importing
-            DrawCsvFileName();
-
-            // Draw the conflict resolution
-            Resolution = (ConflictResolution)EditorGUILayout.IntPopup("Conflict Resolution", ((int)Resolution), ConflictResolutionStrings, ConflictResolutionValues);
-
-            // Draw the file that'll be edited
-            DrawTranslationDictionary();
-
-            // Explain what this dialog does
-            if (ErrorMessage.Length > 0)
+            get
             {
-                EditorGUILayout.HelpBox(ErrorMessage.ToString(), MessageType.Error);
+                return progress.Value;
             }
+            set
+            {
+                progress.Value = value;
+            }
+        }
 
-            DrawProgressBar();
-            EditorGUILayout.EndVertical();
-            GUI.enabled = true;
+        ThreadSafeStringBuilder ProgressMessage
+        {
+            get;
+        } = new ThreadSafeStringBuilder();
+
+        bool IsInMiddleOfImporting
+        {
+            get
+            {
+                return Progress.CompareTo(0f) >= 0;
+            }
         }
 
         GUILayoutOption BrowseButtonHeight
@@ -157,8 +158,64 @@ namespace OmiyaGames.UI.Translations
                 return browseButtonFont;
             }
         }
+        #endregion
 
-        private void DrawCsvFileName()
+        #region Unity Events
+        void OnGUI()
+        {
+            ErrorMessage.Clear();
+            GUI.enabled = !IsInMiddleOfImporting;
+            EditorGUILayout.BeginVertical(GUILayout.ExpandHeight(true));
+            EditorGUILayout.Space();
+
+            // Draw the file we'll be importing
+            MessageType messageType = MessageType.None;
+            DrawCsvFileName(ref messageType);
+
+            // Draw the key column
+            DrawKeyColumnName(ref messageType);
+
+            // Draw the conflict resolution
+            Resolution = (ConflictResolution)EditorGUILayout.IntPopup("Conflict Resolution", ((int)Resolution), ConflictResolutionStrings, ConflictResolutionValues);
+
+            // Draw the file that'll be edited
+            DrawTranslationDictionary();
+
+            // Explain what this dialog does
+            if (ErrorMessage.Length > 0)
+            {
+                EditorGUILayout.HelpBox(ErrorMessage.ToString(), messageType);
+            }
+
+            // Draw the rest of the buttons
+            DrawImportButton();
+            DrawProgressBar();
+            EditorGUILayout.EndVertical();
+            GUI.enabled = true;
+        }
+
+        void OnEnable()
+        {
+            Progress = -1f;
+        }
+
+        void OnInspectorUpdate()
+        {
+            if(IsInMiddleOfImporting == true)
+            {
+                // FIXME: update the message
+                Repaint();
+            }
+        }
+
+        void OnDestroy()
+        {
+            Progress = -1f;
+        }
+        #endregion
+
+        #region UI Helper Methods
+        private void DrawCsvFileName(ref MessageType messageType)
         {
             EditorGUILayout.BeginHorizontal();
 
@@ -172,31 +229,49 @@ namespace OmiyaGames.UI.Translations
             if (GUILayout.Button("Browse...", BrowseButtonFont, BrowseButtonHeight) == true)
             {
                 string newFileName = UnityEditor.EditorUtility.OpenFilePanelWithFilters("Import CSV File", "/Assets/", CsvFileFilter);
-                if(string.IsNullOrEmpty(newFileName) == false)
+                if (string.IsNullOrEmpty(newFileName) == false)
                 {
                     CsvFileName = newFileName;
                 }
             }
 
             // Check if there are any errors
-            if (UnityEngine.Windows.File.Exists(CsvFileName) == false)
+            if (string.IsNullOrEmpty(CsvFileName) == true)
             {
-                if(ErrorMessage.Length > 0)
+                if (ErrorMessage.Length > 0)
                 {
                     ErrorMessage.AppendLine();
                 }
-                ErrorMessage.Append("CSV file '");
+                ErrorMessage.Append("Assign a CSV file to import.");
+                messageType = MessageType.Info;
+            }
+            else if (UnityEngine.Windows.File.Exists(CsvFileName) == false)
+            {
+                if (ErrorMessage.Length > 0)
+                {
+                    ErrorMessage.AppendLine();
+                }
+                ErrorMessage.Append("Cannot import file \"");
                 ErrorMessage.Append(CsvFileName);
-                ErrorMessage.Append("' doesn't exist!");
+                ErrorMessage.Append("\"!");
+                messageType = MessageType.Error;
             }
             EditorGUILayout.EndHorizontal();
         }
 
-        void OnEnable()
+        private void DrawKeyColumnName(ref MessageType messageType)
         {
-
+            KeysColumnName = EditorGUILayout.TextField("Name of the Column with Keys", KeysColumnName);
+            if (string.IsNullOrEmpty(KeysColumnName) == true)
+            {
+                if (ErrorMessage.Length > 0)
+                {
+                    ErrorMessage.AppendLine();
+                }
+                ErrorMessage.Append("Name of the Column with Keys must be defined.");
+                messageType = MessageType.Error;
+            }
         }
-        #endregion
 
         private void DrawTranslationDictionary()
         {
@@ -208,37 +283,127 @@ namespace OmiyaGames.UI.Translations
 
         private void DrawProgressBar()
         {
+            if (IsInMiddleOfImporting == true)
+            {
+                UnityEditor.EditorUtility.DisplayProgressBar("Import CSV Progress", ProgressMessage.ToString(), Progress);
+
+                // TODO: When we're ready, add a method to cancel the import progress
+                //if (UnityEditor.EditorUtility.DisplayCancelableProgressBar("Import CSV Progress", ProgressMessage.ToString(), Progress) == true)
+                //{
+                //    // User canceled, reset progress
+                //    Progress = -1f;
+                //}
+            }
+            else
+            {
+                UnityEditor.EditorUtility.ClearProgressBar();
+            }
+        }
+
+        private void DrawImportButton()
+        {
             // Calculate the size of the progress bar
             EditorGUILayout.Space();
-            Rect bounds = EditorGUILayout.BeginHorizontal(GUILayout.MinHeight(EditorGUIUtility.singleLineHeight));
-            float originalWidth = bounds.width;
-            bounds.x = SideMargin;
-            bounds.width -= (ImportButtonWidth + Space + (SideMargin * 2));
-            bounds.height = EditorGUIUtility.singleLineHeight + Space;
-
-            // Draw the progress bar
-            float displayProgress = Mathf.Clamp01(Progress);
-            EditorGUI.ProgressBar(bounds, displayProgress, displayProgress.ToString("0%"));
 
             // Only enable if no error messages are there
             bool isEnabled = GUI.enabled;
-            GUI.enabled = ((ErrorMessage.Length == 0) && (Progress < 0f));
-
-            // Calculate the size of the button
-            bounds.x += (bounds.width + Space);
-            bounds.width = ImportButtonWidth;
+            GUI.enabled = ((ErrorMessage.Length == 0) && (IsInMiddleOfImporting == false));
 
             // Draw the button
-            if (GUI.Button(bounds, "Import") == true)
+            if ((GUILayout.Button("Import") == true) && (GUI.enabled == true))
             {
-                // FIXME: do soemthing!
+                // Reset the progress
+                Progress = 0f;
+
+                // Start a new thread
+                ThreadPool.QueueUserWorkItem(ImportCsvFile);
             }
 
             // Revert enable behavior
             GUI.enabled = isEnabled;
+        }
+        #endregion
 
-            // Close the layout
-            EditorGUILayout.EndVertical();
+        private void ImportCsvFile(object stateInfo)
+        {
+            // Indicate we're processing something!
+            Progress = 0f;
+
+            List<Dictionary<string, string>> results = null;
+            try
+            {
+                results = CSVReader.ReadFile(CsvFileName, csvReadStatus);
+            }
+            catch (System.Exception ex)
+            {
+                // FIXME: indicate the error that the file could not be read
+                results = null;
+            }
+
+            // Check if we have any results
+            // FIXME: report progress, too!
+            if(results != null)
+            {
+                ProcessResults(results);
+            }
+        }
+
+        private void ProcessResults(List<Dictionary<string, string>> results)
+        {
+            // FIXME: report progress, too!
+            // Check if we want to overwrite the translations
+            if (Resolution == ConflictResolution.Overwrite)
+            {
+                // If so, completely clear the translations
+                DictionaryToEdit.AllTranslations.Clear();
+            }
+
+            // Go through each row of the results
+            string key;
+            Dictionary<int, string> translations;
+            foreach (Dictionary<string, string> row in results)
+            {
+                // First, search for the key column
+                if ((row != null) && (row.TryGetValue(KeysColumnName, out key) == true))
+                {
+                    // Check if this key introduces a conflict
+                    translations = null;
+                    if (DictionaryToEdit.AllTranslations.ContainsKey(key) == false)
+                    {
+                        // If no conflicts, add a new translations dictionary
+                        translations = new Dictionary<int, string>(row.Count - 1);
+                        DictionaryToEdit.AllTranslations.Add(key, translations);
+                    }
+                    else if (Resolution == ConflictResolution.AppendIgnore)
+                    {
+                        // If we're ignoring conflicts, skip this row.
+                        continue;
+                    }
+                    else
+                    {
+                        // Otherwise, overwrite the key with a new translations dictionary
+                        translations = new Dictionary<int, string>(row.Count - 1);
+                        DictionaryToEdit.AllTranslations[key] = translations;
+                    }
+
+                    // Go through each cell in each row
+                    foreach (KeyValuePair<string, string> cell in row)
+                    {
+                        // Make sure the language list is supported
+                        if (DictionaryToEdit.SupportedLanguages.Contains(cell.Key) == true)
+                        {
+                            // If so, add it and its text into the translations dictionary
+                            translations.Add(DictionaryToEdit.SupportedLanguages[cell.Key], cell.Value);
+                        }
+                    }
+                }
+            }
+
+            // Apply the changes
+            DictionaryToEdit.UpdateSerializedTranslations();
+
+            // Indicate we're done
+            Progress = 1f;
         }
     }
 }
