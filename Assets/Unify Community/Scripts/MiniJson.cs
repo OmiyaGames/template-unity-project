b6779025f0e3fc6c1a5bdc9e1869745b046cb58b
Taro Omiya <taro.omiya@omiyagames.com>
2018-10-12 04:53:44 -0400

    Renaming HSBColor and MiniJSON to be more camelcase.

--- Assets/Unify Community/Scripts/MiniJson.cs
+++ Assets/Unify Community/Scripts/MiniJson.cs
@@ -6,41 +6,8 @@ using System.Text;
 
 namespace Community
 {
-    // Example usage:
-    //
-    //  using UnityEngine;
-    //  using System.Collections;
-    //  using System.Collections.Generic;
-    //  using MiniJSON;
-    //
-    //  public class MiniJSONTest : MonoBehaviour {
-    //      void Start () {
-    //          var jsonString = "{ \"array\": [1.44,2,3], " +
-    //                          "\"object\": {\"key1\":\"value1\", \"key2\":256}, " +
-    //                          "\"string\": \"The quick brown fox \\\"jumps\\\" over the lazy dog \", " +
-    //                          "\"unicode\": \"\\u3041 Men\u00fa sesi\u00f3n\", " +
-    //                          "\"int\": 65536, " +
-    //                          "\"float\": 3.1415926, " +
-    //                          "\"bool\": true, " +
-    //                          "\"null\": null }";
-    //
-    //          var dict = Json.Deserialize(jsonString) as Dictionary<string,object>;
-    //
-    //          Debug.Log("deserialized: " + dict.GetType());
-    //          Debug.Log("dict['array'][0]: " + ((List<object>) dict["array"])[0]);
-    //          Debug.Log("dict['string']: " + (string) dict["string"]);
-    //          Debug.Log("dict['float']: " + (double) dict["float"]); // floats come out as doubles
-    //          Debug.Log("dict['int']: " + (long) dict["int"]); // ints come out as longs
-    //          Debug.Log("dict['unicode']: " + (string) dict["unicode"]);
-    //
-    //          var str = Json.Serialize(dict);
-    //
-    //          Debug.Log("serialized: " + str);
-    //      }
-    //  }
-
     ///-----------------------------------------------------------------------
-    /// <copyright file="Json.cs" company="Omiya Games">
+    /// <copyright file="MiniJson.cs">
     /// Copyright (c) 2013 Calvin Rien
     /// From: https://gist.github.com/darktable/1411710
     ///
@@ -76,6 +43,41 @@ namespace Community
     /// JSON uses Arrays and Objects. These correspond here to the datatypes IList and IDictionary.
     /// All numbers are parsed to doubles.
     /// </summary>
+    /// <remarks>
+    /// Example usage:
+    /// <code>
+    /// using UnityEngine;
+    /// using System.Collections;
+    /// using System.Collections.Generic;
+    /// using MiniJSON;
+    ///
+    /// public class MiniJSONTest : MonoBehaviour {
+    ///     void Start () {
+    ///         var jsonString = "{ \"array\": [1.44,2,3], " +
+    ///                         "\"object\": {\"key1\":\"value1\", \"key2\":256}, " +
+    ///                         "\"string\": \"The quick brown fox \\\"jumps\\\" over the lazy dog \", " +
+    ///                         "\"unicode\": \"\\u3041 Men\u00fa sesi\u00f3n\", " +
+    ///                         "\"int\": 65536, " +
+    ///                         "\"float\": 3.1415926, " +
+    ///                         "\"bool\": true, " +
+    ///                         "\"null\": null }";
+    ///
+    ///         var dict = Json.Deserialize(jsonString) as Dictionary<string,object>;
+    ///
+    ///         Debug.Log("deserialized: " + dict.GetType());
+    ///         Debug.Log("dict['array'][0]: " + ((List<object>) dict["array"])[0]);
+    ///         Debug.Log("dict['string']: " + (string) dict["string"]);
+    ///         Debug.Log("dict['float']: " + (double) dict["float"]); // floats come out as doubles
+    ///         Debug.Log("dict['int']: " + (long) dict["int"]); // ints come out as longs
+    ///         Debug.Log("dict['unicode']: " + (string) dict["unicode"]);
+    ///
+    ///         var str = Json.Serialize(dict);
+    ///
+    ///         Debug.Log("serialized: " + str);
+    ///     }
+    /// }
+    /// </code>
+    /// </remarks>
     public static class Json
     {
         /// <summary>
