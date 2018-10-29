3f9ee6809d281e232a25bc6dfc75b4672dd00413
Taro Omiya <taro.omiya@omiyagames.com>
2018-10-29 06:43:53 -0400

    Working on building a framework for BuildSettings.

---
+++ Assets/Omiya Games/Scripts/Build Settings/IBuildSetting.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace OmiyaGames.Builds
+{
+    ///-----------------------------------------------------------------------
+    /// <copyright file="IBuildSetting.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2018 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>10/29/2018</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// Assets holding settings for creating builds.
+    /// </summary>
+    public abstract class IBuildSetting : ScriptableObject
+    {
+        public struct BuildResult
+        {
+            public enum Status
+            {
+                Info,
+                Success,
+                Warning,
+                Error
+            }
+
+            public BuildResult(Status state, string message)
+            {
+                State = state;
+                Message = message;
+            }
+
+            public Status State
+            {
+                get;
+            }
+
+            public string Message
+            {
+                get;
+            }
+        }
+
+        /// <summary>
+        /// Returns the maximum number of results possible,
+        /// returned by a single build call.
+        /// </summary>
+        public abstract int MaxNumberOfResults
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Recursively creates builds.
+        /// </summary>
+        /// <param name="results">List of statuses indicating the results</param>
+        /// <returns></returns>
+        public bool Build(out List<BuildResult> results)
+        {
+            return Build(null, out results);
+        }
+
+        /// <summary>
+        /// Recursively creates builds.
+        /// </summary>
+        /// <param name="results">List of statuses indicating the results</param>
+        /// <returns></returns>
+        public bool Build(RootBuildSetting root, out List<BuildResult> results)
+        {
+            results = new List<BuildResult>(MaxNumberOfResults);
+            return BuildBaseOnSettings(root, ref results);
+        }
+
+        /// <summary>
+        /// Creates builds specific to this setting.
+        /// </summary>
+        /// <param name="results">List of statuses indicating the results</param>
+        /// <returns>True if the build was successful.</returns>
+        protected abstract bool BuildBaseOnSettings(RootBuildSetting root, ref List<BuildResult> results);
+
+        #region Helper Methods
+        /// <summary>
+        /// Helper method to build a list of settings.
+        /// </summary>
+        /// <param name="settings">All the build settings to build from.</param>
+        /// <param name="results">List of statuses indicating the results</param>
+        /// <returns>True if the build was successful.</returns>
+        protected static bool BuildGroup(RootBuildSetting root, IList<IBuildSetting> settings, ref List<BuildResult> results)
+        {
+            bool returnFlag = true;
+
+            // Go through all settings
+            foreach (IChildBuildSetting setting in settings)
+            {
+                // Attempt to make a build
+                if (setting.BuildBaseOnSettings(root, ref results) == false)
+                {
+                    // If the build failed, update return flag
+                    returnFlag = false;
+
+                    // Check if we need to halt
+                    if (setting.RootSetting.HaltOnFirstError == true)
+                    {
+                        break;
+                    }
+                }
+            }
+            return returnFlag;
+        }
+
+        protected static void AddSetting(IBuildSetting parent, List<IBuildSetting> allSettings, IBuildSetting setting)
+        {
+            if(setting is IChildBuildSetting)
+            {
+                ((IChildBuildSetting)setting).Parent = parent;
+            }
+            allSettings.Add(setting);
+        }
+
+        protected static IBuildSetting RemoveSetting(List<IBuildSetting> allSettings, int index)
+        {
+            // Grab the return value
+            IBuildSetting returnChild = allSettings[index];
+
+            // Reset parent
+            if (returnChild is IChildBuildSetting)
+            {
+                ((IChildBuildSetting)returnChild).Parent = null;
+            }
+
+            // Remove the element
+            allSettings.RemoveAt(index);
+            return returnChild;
+        }
+        #endregion
+    }
+}
---
+++ Assets/Omiya Games/Scripts/Build Settings/RootBuildSetting.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace OmiyaGames.Builds
+{
+    ///-----------------------------------------------------------------------
+    /// <copyright file="RootBuildSetting.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2018 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>10/29/2018</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// Assets holding settings for creating builds.
+    /// </summary>
+    public class RootBuildSetting : IBuildSetting
+    {
+        [SerializeField]
+        private string buildPath;
+        [SerializeField]
+        private bool haltOnFirstError = false;
+        [SerializeField]
+        List<IBuildSetting> allSettings = new List<IBuildSetting>();
+
+        public override int MaxNumberOfResults
+        {
+            get
+            {
+                // Indicate this group drops 2 statuses
+                int returnNumber = 2;
+
+                // Increment the max by all the other settings
+                foreach (IBuildSetting setting in allSettings)
+                {
+                    returnNumber += setting.MaxNumberOfResults;
+                }
+                return returnNumber;
+            }
+        }
+
+        public string BuildPath
+        {
+            get
+            {
+                return buildPath;
+            }
+        }
+
+        public bool HaltOnFirstError
+        {
+            get
+            {
+                return haltOnFirstError;
+            }
+        }
+
+        public void Add(IBuildSetting addSetting)
+        {
+            AddSetting(this, allSettings, addSetting);
+        }
+
+        public IBuildSetting Remove(int index)
+        {
+            return RemoveSetting(allSettings, index);
+        }
+
+        protected override bool BuildBaseOnSettings(RootBuildSetting root, ref List<BuildResult> results)
+        {
+            // Check whether root is null or not
+            if (root == null)
+            {
+                // If so, replace it with this variable
+                root = this;
+            }
+
+            // Indicate group build started
+            results.Add(new BuildResult(BuildResult.Status.Info, "Going through " + name));
+
+            // Build the list of settings
+            bool returnFlag = BuildGroup(root, allSettings, ref results);
+
+            // Indicate group build ended
+            results.Add(new BuildResult(BuildResult.Status.Info, "Finished building " + name));
+            return returnFlag;
+        }
+    }
+}
