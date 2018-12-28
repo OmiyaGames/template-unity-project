958812c6d31eb1c5dc79a5faab029770f0bda0fc
Taro Omiya <taro.omiya@omiyagames.com>
2018-12-28 01:47:43 -0500

    Fixed issue #67: added a new ScenePath attribute one can add to strings very easily.  Forces the string field to only accept scene files, and sets the string to the path of that scene.

---
+++ Assets/Omiya Games/Scripts/Attributes/ScenePathAttribute.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System;
+
+namespace OmiyaGames
+{
+    ///-----------------------------------------------------------------------
+    /// <copyright file="ScenePathAttribute.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2018 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>12/28/2018</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// Makes a string field accept scene assets only.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+    public class ScenePathAttribute : PropertyAttribute
+    {
+        /// <summary>
+        /// Adds a browse button to a string field.
+        /// </summary>
+        /// <param name="fileExtensions">The file extention, without "*."</param>
+        /// <param name="defaultPath"></param>
+        public ScenePathAttribute()
+        {
+        }
+    }
+}
