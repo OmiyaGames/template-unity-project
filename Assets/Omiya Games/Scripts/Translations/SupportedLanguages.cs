3f4c68862a922a47e4aa1ce1375cd14d12989c61
Taro Omiya <taro.omiya@omiyagames.com>
2019-05-28 02:34:23 -0400

    Writing notes to myself for future improvements with the translations framework.  To be fair, it may go out-of-date by the time I get to it, since it seems Unity is already working on a package more extensive than this one.

--- Assets/Omiya Games/Scripts/Translations/SupportedLanguages.cs
+++ Assets/Omiya Games/Scripts/Translations/SupportedLanguages.cs
@@ -3,8 +3,41 @@ using UnityEngine;
 using System;
 using TMPro;
 
+// FIXME: look into having each language hold a unique ID,
+// and make TranslatedDictionary.cs store that ID per translation.
+// This way, if a user deletes a language, that ID remains unchanged.
 namespace OmiyaGames.Translations
 {
+    ///-----------------------------------------------------------------------
+    /// <copyright file="SupportedLanguages.cs" company="Omiya Games">
+    /// The MIT License (MIT)
+    /// 
+    /// Copyright (c) 2014-2019 Omiya Games
+    /// 
+    /// Permission is hereby granted, free of charge, to any person obtaining a copy
+    /// of this software and associated documentation files (the "Software"), to deal
+    /// in the Software without restriction, including without limitation the rights
+    /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    /// copies of the Software, and to permit persons to whom the Software is
+    /// furnished to do so, subject to the following conditions:
+    /// 
+    /// The above copyright notice and this permission notice shall be included in
+    /// all copies or substantial portions of the Software.
+    /// 
+    /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    /// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    /// THE SOFTWARE.
+    /// </copyright>
+    /// <author>Taro Omiya</author>
+    /// <date>10/10/2018</date>
+    ///-----------------------------------------------------------------------
+    /// <summary>
+    /// Assets holding a list of languages and their information.
+    /// </summary>
     public class SupportedLanguages : ScriptableObject
     {
         public const string DefaultLanguageName = "English (USA)";
